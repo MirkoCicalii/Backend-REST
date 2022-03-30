ed66635d35847038de9266c50b305ed11cee4c73
lai2-01 <lai2-01@itismeucci.com>
2022-03-30 13:35:01 +0200

    ok

--- connection.php
+++ connection.php
@@ -1,9 +1,10 @@
 <?php
    $servername= "172.17.0.1:3306";
    $user= "root";
+   $pass = "1234";
    $db= "mydb";
 
    //Create Connection
-   $conn = mysqli_connect($servername, $user, $db) or die("Connessione non riuscita".mysqli_connect_error());
+   $conn = mysqli_connect($servername, $user,$pass, $db) or die("Connessione non riuscita".mysqli_connect_error());
 
 ?>
