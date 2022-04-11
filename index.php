398dd58939cb6d222d171b6b40ea5923ea328cb7
lai2-01 <lai2-01@itismeucci.com>
2022-04-11 09:54:37 +0200

    OK

--- index.php
+++ index.php
@@ -19,20 +19,20 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 $data = ['id' => 10001, 'birthDate' => "1953-09-01", 'firstName' => 'Georgi', 'lastName' => 'Facello', 'gender' => 'M', 'hireDate' => '1986-06-25'];
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+if ($metodo === 'GET') {
     echo json_encode($data);
     $sql = "SELECT * from employees";
     $result = $conn->query($sql);
     // echo ($result);
     // return $result;
 } else 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($metodo === 'POST') {
     echo json_encode($data);
 } else 
-    if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+    if ($metodo === 'DELETE') {
     echo json_encode($data);
 } else 
-    if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+    if ($metodo === 'PUT') {
     echo json_encode($data);
 }
 
