1e28af99d8be1c5eefc877db6a145826c710388b
lai2-01 <lai2-01@itismeucci.com>
2022-04-11 09:32:56 +0200

    ok

--- index.php
+++ index.php
@@ -9,27 +9,6 @@ $servername = "172.17.0.1:3306";
     // Create connection
     $conn = mysqli_connect($servername, $user, $pass, $db) or die("Connessione non riuscita". mysqli_connect_error());
 
-  
-    //if ($metodo == "GET"){
-    
-   //echo "riccabello";
-
-  //}
-    //if ($metodo == "POST"){
-    
-    //echo "Sì, mi è arrivata un POST!";
-  //}
-    //if ($metodo == "PUT"){
-    
-    //echo "Sì, mi è arrivata un PUT!";
-  //}
-    //if ($metodo == "DELETE"){
-    
-   // echo "Sì, mi è arrivata una DELETE!";
-  //}
-        
-
-
 
 
 header("Access-Control-Allow-Origin: *");
